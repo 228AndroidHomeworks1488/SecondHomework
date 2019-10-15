5f559d52b8da77e9e2e3a1018ca9a527eba7f65c
Alexander Sukhikh <xbox24_772@mail.ru>
2019-10-15 22:46:56 +0300

    final

--- app/src/main/java/bonch/dev/secondhomework/FragmentActivity.kt
+++ app/src/main/java/bonch/dev/secondhomework/FragmentActivity.kt
@@ -1,7 +1,34 @@
 package bonch.dev.secondhomework
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import bonch.dev.secondhomework.fragments.FirstFragment
+import bonch.dev.secondhomework.fragments.SecondFragment
 
 class FragmentActivity: AppCompatActivity() {
+
+    val fragmentManager = supportFragmentManager
+    val firstFragmentName = "FIRST FRAGMENT"
+    val secondFragmentName = "SECOND FRAGMENT"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+        val firstFragment = FirstFragment()
+        fragmentManager
+            .beginTransaction()
+            .add(R.id.fragment, firstFragment)
+            //.addToBackStack(firstFragmentName)
+            .commit()
+    }
+
+    fun attachSecondFragment(){
+        val secondFragment = SecondFragment()
+        fragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment, secondFragment)
+            .addToBackStack(secondFragmentName)
+            .commit()
+    }
 
 }
---
+++ app/src/main/java/bonch/dev/secondhomework/fragments/SecondFragment.kt
@@ -0,0 +1,23 @@
+package bonch.dev.secondhomework.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import bonch.dev.secondhomework.R
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+
+}
+
